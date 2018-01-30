662c744836a6d434389c118d97f04fe75b50d95a
michael-38 <michael.38@outlook.com>
2018-01-30 19:21:19 +0000

    added functionality of posting and displaying tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,32 +19,79 @@
     }
   })
 
+  $("form").on('submit', function(event) {
+    event.preventDefault();
+    if ($("textarea").val().length === 0) {
+      alert("Please enter something")
+    } else if ($("textarea").val().length > 140) {
+      alert("Enter a shorter tweet");
+    } else {
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: $(this).serialize(),
+        success: loadTweets
+      })
+    }
+  });
+
+
+  function loadTweets () {
+    console.log("performing ajax call...");
+    $.ajax({  //make Ajax GET request to /tweets and receive the array of tweets as JSON
+      url: '/tweets',
+      method: 'GET',
+      success: renderTweets
+    });
+  };
+
+  loadTweets(); //load tweets from db
+
+
+
+
+
+
+
 });
 
 
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-
 function createTweetElement(obj) {
+  let profile_pic = obj["user"].avatars.small;
+  let fullname = obj["user"].name;
+  let handle = obj["user"].handle;
+  let tweet_text = obj["content"].text;
+  let created_at = obj["created_at"];
 
+  $tweet = (`
+    <article class="tweet">
+    <header>
+    <img class="profile-pic" src="${profile_pic}">
+    <span id="fullname"> ${fullname} </span>
+    <span id="handle">${handle}</span>
+    </header>
+    <p id="tweet-text">${tweet_text}</p>
+    <footer>
+    <span id="created-at">${created_at}</span>
+    <div id="icons">
+    <img id="flag" src="/images/flag.png">
+    <img id="retweet" src="/images/retweet.png">
+    <img id="like" src="/images/like.png">
+    </div>
+    </footer>
+    </article>
+    `);
+
+  return $tweet;
 }
 
 
+function renderTweets(data) {
+  for (tweet of data) {
+    $("#all-tweets").append(createTweetElement(tweet));
+  }
+}
 
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-    $(".new-tweet textarea").on('keypress', function() {
+    $(".new-tweet textarea").on('input', function() {
         // var charCount = this.val()
         // console.log($(".new-tweet textarea").length); //this only totals to "1" every time a key is pressed
-        var charCount = $(this).val().length + 1;
+        var charCount = $(this).val().length;
         var charLeft = 140 - charCount;
         console.log(charLeft); //this totals the correct number of characters within textarea
-        $(".new-tweet .counter").text(charLeft);
+        // $(".new-tweet .counter").text("Character(s) remaining: " + charLeft);
+        $(this).find(".counter").text("Character(s) remaining: " + charLeft);
 
         if (charLeft < 0) {
           $(".new-tweet .counter").addClass("overlimit");
